375b2fdddbdbb34d86bb0aeffcb8cdec79f7bef7
ananyaAdhikary <ananyaadhikary02@gmail.com>
2024-06-25 22:50:22 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggler() {
+    var x = document.getElementById("nav-cover2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
